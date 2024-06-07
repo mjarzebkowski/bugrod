0ee9210cfab6f8e16f997238397804bcbf2f6a93
Maciej <maciej.jarzebkowski@gmail.com>
2024-06-07 19:26:32 +0200

    Add Ui base

---
+++ BugRod.Ui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BugRod.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
