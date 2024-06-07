a7b462d4abb5c3611d3386d8331e117ecd1074a3
Maciej <maciej.jarzebkowski@gmail.com>
2024-06-07 20:15:17 +0200

    add base tests

---
+++ BugRod.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+namespace BugRod.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Connect()
+        {
+
+        }
+
+        [Fact]
+        public void Disconnect()
+        {
+            
+        }
+
+        [Fact]
+        public void AddIssue()
+        {
+
+        }
+
+        [Fact]
+        public void ModifyIssue()
+        {
+
+        }
+
+        [Fact]
+        public void CloseIssue()
+        {
+
+        }
+
+
+
+    }
+}
