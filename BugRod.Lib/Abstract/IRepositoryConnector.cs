e14b9b315792edcf064098e751560863b90cfa7d
Maciej <maciej.jarzebkowski@gmail.com>
2024-06-07 20:14:57 +0200

    Add base lib project

---
+++ BugRod.Lib/Abstract/IRepositoryConnector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugRod.Lib.Containers;
+
+namespace BugRod.Lib.Abstract
+{
+    internal interface IRepositoryConnector
+    {
+        // General
+        Task<OperationStatus> ConnectToService(Repository repository);
+        Task<OperationStatus> DisconnectService(Repository repository);
+
+        // Manage
+        Task<OperationStatus> AddIssue(Repository repository, Issue bug);
+        Task<OperationStatus> ModifyIssue(Repository repository, Issue bug);
+        Task<OperationStatus> CloseIssue(Repository repository, Issue bug);
+        Task<IEnumerable<OperationStatus>> ExportIssue(Repository sourceRepository, IEnumerable<Issue> bugs);
+        Task<IEnumerable<OperationStatus>> ImportIssue(Repository destinationRepository, File fromFile);
+    }
+
+    enum OperationStatus : byte
+    {
+        Success = 1,
+        Fail    = 2
+    }
+}
---
+++ BugRod.Lib/Containers/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugRod.Lib.Containers
+{
+    internal class Repository
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string WebAddress { get; set; } // IP or WebName including port
+
+
+    }
+
+    enum AuthenticationType : byte
+    {
+        Token           = 1,
+        LoginPassword   = 2,
+    }
+}
